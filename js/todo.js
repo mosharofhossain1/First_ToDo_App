d470b6dcd64e4377d252c1c5178871cf850a1baf
Mosharof <md5470415@gmail.com>
2024-03-30 22:52:12 +0600

    application update Again

--- js/todo.js
+++ js/todo.js
@@ -11,12 +11,21 @@ const listAdd = document.querySelector('.add_list');
 addBtn.addEventListener('click', function(){
     const text = inputField.value;
     if(text){
+       
         const li = document.createElement('li');
         li.style.listStyleType = 'hiden'
-
-        li.innerHTML = `${text} <button id='delete'>X</button>`;
+        li.style.background  = 'gray'
+        li.style.color = 'white'
+        li.style.padding  = '6px'
+        li.style.width = 'calc(100% - 200px)'
+        li.style.borderRadius = '7px'
+        li.style.marginBottom = '20px' 
+        li.innerHTML = `${text} <button style='float:right;' id='delete'>X</button>`;
         listAdd.append(li);
         inputField.value = '';
+    }
+    else{
+        alert('Please Provide Valid Task & Then Click Btn')
     }
     
 });
@@ -33,13 +42,19 @@ listAdd.addEventListener('click', function(event){
 // Step:- 4
 inputField.addEventListener('keydown', function(e){
     if(e.key === 'Enter'){
-        
         const text = inputField.value;
         const li = document.createElement('li');
         li.style.listStyleType = 'hiden'
-        li.innerHTML = `${text} <button id='delete'>X</button>`
+        li.style.background  = 'gray'
+        li.style.color = 'white'
+        li.style.padding  = '6px'
+        li.style.width = 'calc(100% - 200px)'
+        li.style.borderRadius = '7px'
+        li.style.marginBottom = '20px' 
+        li.innerHTML = `${text} <button style='float:right;' id='delete'>X</button>`;
         listAdd.append(li);
         inputField.value = '';
     }
+    
 })
  
